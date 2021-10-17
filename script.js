5f28437a7176393eef980b89a29f254eda90b8ff
SzkolenieFrontEnd <92541764+SzkolenieFrontEnd@users.noreply.github.com>
2021-10-17 18:29:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//1.
+// Za pomocą pętli for wypisz w konsoli wszystkie zwierzęta z tablcy
+const animals = ['pies','koza','małpa','kot','słoń'];
+
+
+//2. 
+//Za pomocą for wyświetl informacje na którym miejscu parkingowym znajduje się
+// dany samochód (niech numerkami miejsc bedą numery indeksów)
+const cars = ['Opel Astra', 'Dacia Duster', 'Skoda Octavia', 'BMWe46']
+
+//3.
+// Losuj owoce tak długo aż nie trafisz banana, dodatkowo jeśli trafisz buraka uzyj warunku if i wyświetl informacje że to nie owoc
+// losuj poszczególne elementy tablicy za pomocą math random i zapisuj je w result 
+// z kazda iteracją pętli zwiększaj zmienną iters o 1,
+// Ostatecznie wyświetl tekst o wylosowaniu banana i informacje za którym razem udało się go wylosować
+const fruits = ['jabłko', 'banan', 'gruszka', 'burak'];
+let result = null; 
+let iters = 0; 
+
+
+
+//4.
+//Do każdego przedmiotu w koszyku dodaj napis premium, wykorzystaj forEach
+const checkoutItems = ["telewizor","marchewka","perfumy","klawiatura"];
+
+checkoutItems.forEach(item => {
+    item = `${item} premium`;
+    console.log(item);
+})
+
+
+
+//5.
+// Stwórz dwie pętle. W jednej wypisz wszystkie nicki z tablicy, a w drugiej ich indeksy, wykorzystaj for...in i for...of
+const users = ['Jim99','EddieX','Ray2000'];
+
+
+
+
